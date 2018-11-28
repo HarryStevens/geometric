69c5d59da4b27d2f003fa7e5e11dc8fb29453f36
Harry Stevens <harryjosephstevens@gmail.com>
2018-11-28 11:50:45 -0500

    Add a semicolon

--- src/distance.js
+++ src/distance.js
@@ -2,5 +2,5 @@
 // Takes two arguments, each of which is a point represented as an array of two numbers,
 // where the first number is its x coordinate and the second number is its y coordinate.
 export default function distance(a, b){
-  return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2))
+  return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2));
 }
