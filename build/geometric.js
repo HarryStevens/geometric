72d7835f42f5480c2b36791d0d2b9a6941fd2616
Harry Stevens <harryjosephstevens@gmail.com>
2018-12-02 02:41:59 -0500

    Version 0.0.9

--- build/geometric.js
+++ build/geometric.js
@@ -1,4 +1,4 @@
-// https://github.com/HarryStevens/geometric#readme Version 0.0.8. Copyright 2018 Harry Stevens.
+// https://github.com/HarryStevens/geometric#readme Version 0.0.9. Copyright 2018 Harry Stevens.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -78,9 +78,9 @@
 
     else {
       // See: https://math.stackexchange.com/questions/1964905/rotation-around-non-zero-point
-      var p0 = p.map((c, i) => c - o[i]);
+      var p0 = p.map(function(c, i){ return c - o[i]; });
       var r = rotate(p0, a);
-      return r.map((c, i) => c + o[i]);
+      return r.map(function(c, i){ return c + o[i]; });
     }
     
     function rotate(p, a){
@@ -97,11 +97,11 @@
 
     else {
       // See: https://math.stackexchange.com/questions/1964905/rotation-around-non-zero-point
-      var p0 = p.map((c, i) => c - o[i]);
+      var p0 = p.map(function(c, i){ return c - o[i]; });
       var r = rotate(p0, a);
-      return r.map((c, i) => c + o[i]);
+      return r.map(function(c, i){ return c + o[i]; });
     }
-    
+      
     function rotate(p, a){
       // See: https://en.wikipedia.org/wiki/Cartesian_coordinate_system#Rotation
       return [(p[0] * Math.cos(a)) - p[1] * Math.sin(a), (p[0] * Math.sin(a)) + p[1] * Math.cos(a)];
