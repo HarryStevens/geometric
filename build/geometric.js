9a276c6a529f0a9277f01debfd70817f2a1d4a8d
Harry Stevens <harryjosephstevens@gmail.com>
2019-01-21 13:18:28 -0500

    Bump to 1.0.5

--- build/geometric.js
+++ build/geometric.js
@@ -1,4 +1,4 @@
-// https://github.com/HarryStevens/geometric#readme Version 1.0.4. Copyright 2019 Harry Stevens.
+// https://github.com/HarryStevens/geometric#readme Version 1.0.5. Copyright 2019 Harry Stevens.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
