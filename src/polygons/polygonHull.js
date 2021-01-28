21bb93d916d8c8728d72f716e13774444791edea
HarryStevens <harryjosephstevens@gmail.com>
2021-01-27 20:23:26 -0500

    Simplify polygonHull algorithm implementation

--- src/polygons/polygonHull.js
+++ src/polygons/polygonHull.js
@@ -8,19 +8,19 @@ export function polygonHull(points){
   const pointsCopy = points.slice().sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
 
   let lower = [];
-  for (let i0 = 0; i0 < pointsCopy.length; i0++) {
-    while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], pointsCopy[i0]) <= 0) {
+  for (let i = 0; i < pointsCopy.length; i++) {
+    while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], pointsCopy[i]) <= 0) {
        lower.pop();
     }
-    lower.push(pointsCopy[i0]);
+    lower.push(pointsCopy[i]);
   }
 
   let upper = [];
-  for (let i1 = pointsCopy.length - 1; i1 >= 0; i1--) {
-    while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], pointsCopy[i1]) <= 0) {
+  for (let i = pointsCopy.length - 1; i >= 0; i--) {
+    while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], pointsCopy[i]) <= 0) {
        upper.pop();
     }
-    upper.push(pointsCopy[i1]);
+    upper.push(pointsCopy[i]);
   }
 
   upper.pop();
