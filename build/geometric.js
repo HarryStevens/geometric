c5cd0b2c126f1930ab8e7bbca8937db8ea92e81a
HarryStevens <harryjosephstevens@gmail.com>
2021-01-27 20:24:18 -0500

    2.2.6

--- build/geometric.js
+++ build/geometric.js
@@ -1,4 +1,4 @@
-// https://github.com/HarryStevens/geometric#readme Version 2.2.5. Copyright 2020 Harry Stevens.
+// https://github.com/HarryStevens/geometric#readme Version 2.2.6. Copyright 2021 Harry Stevens.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -149,22 +149,22 @@
     });
     var lower = [];
 
-    for (var i0 = 0; i0 < pointsCopy.length; i0++) {
-      while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], pointsCopy[i0]) <= 0) {
+    for (var i = 0; i < pointsCopy.length; i++) {
+      while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], pointsCopy[i]) <= 0) {
         lower.pop();
       }
 
-      lower.push(pointsCopy[i0]);
+      lower.push(pointsCopy[i]);
     }
 
     var upper = [];
 
-    for (var i1 = pointsCopy.length - 1; i1 >= 0; i1--) {
-      while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], pointsCopy[i1]) <= 0) {
+    for (var _i = pointsCopy.length - 1; _i >= 0; _i--) {
+      while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], pointsCopy[_i]) <= 0) {
         upper.pop();
       }
 
-      upper.push(pointsCopy[i1]);
+      upper.push(pointsCopy[_i]);
     }
 
     upper.pop();
