418d35743f18724da96135deaf2a014682787a22
Yann Briançon <ybr@outsight.tech>
2021-07-15 14:29:39 +0200

    Fix by rounding to a 15 fraction digits precision

--- src/relationships/lineIntersectsLine.js
+++ src/relationships/lineIntersectsLine.js
@@ -2,9 +2,6 @@
 // See: https://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function/24392281#24392281
 // Returns a boolean.
 export function lineIntersectsLine(lineA, lineB) {
-  // First test to see if the lines share an endpoint
-  if (sharePoint(lineA, lineB)) return true;
-
   let a = lineA[0][0],
       b = lineA[0][1],
       c = lineA[1][0],
@@ -15,29 +12,17 @@ export function lineIntersectsLine(lineA, lineB) {
       s = lineB[1][1],
       det, gamma, lambda;
 
-  det = (c - a) * (s - q) - (r - p) * (d - b);
-  if (det === 0) {
-    return false;
+  det = ((c - a) * (s - q) - (r - p) * (d - b));
+  
+  if (floatEqual(det, 0)) {
+      return false;
   } else {
-    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    lambda = (((s - q) * (r - a) + (p - r) * (s - b)) / det);
+    gamma = (((b - d) * (r - a) + (c - a) * (s - b)) / det);
+    return (0 <= lambda + Number.EPSILON  && lambda <= 1 + Number.EPSILON ) && (0 <= gamma + Number.EPSILON && gamma <= 1 + Number.EPSILON);
   }
 }
 
-function sharePoint(lineA, lineB){
-  let share = false;
-  for (let i = 0; i < 2; i++){
-    for (let j = 0; j < 2; j++){
-      if (equal(lineA[i], lineB[j])){
-        share = true;
-        break;
-      }
-    }
-  }
-  return share;
-}
-
-function equal(pointA, pointB){
-  return pointA[0] === pointB[0] && pointA[1] === pointB[1];
+function floatEqual(float1, float2) { 
+  return float1 <= float2 + Number.EPSILON && float1 >= float2 - Number.EPSILON;
 }
