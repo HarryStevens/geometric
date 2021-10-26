8a69687dd50616660d5eb46c1e5ee8eccdcc20fc
J.C <jcyuan.space@gmail.com>
2021-10-26 14:54:31 +0800

    move the helper function outside of the closure
    improve

--- src/points/pointRotate.js
+++ src/points/pointRotate.js
@@ -1,5 +1,10 @@
 import { angleToRadians } from "../angles/angleToRadians";
 
+function rotate(point, angle){
+    // See: https://en.wikipedia.org/wiki/Cartesian_coordinate_system#Rotation
+    return [(point[0] * Math.cos(angle)) - point[1] * Math.sin(angle), (point[0] * Math.sin(angle)) + point[1] * Math.cos(angle)];
+}
+
 // Rotates a point by an angle in degrees around an origin.
 export function pointRotate(point, angle, origin){
   const r = angleToRadians(angle);
@@ -12,10 +17,5 @@ export function pointRotate(point, angle, origin){
     const p0 = point.map((c, i) => c - origin[i]);
     const rotated = rotate(p0, r);
     return rotated.map((c, i) => c + origin[i]);
-  }
-  
-  function rotate(point, angle){
-    // See: https://en.wikipedia.org/wiki/Cartesian_coordinate_system#Rotation
-    return [(point[0] * Math.cos(angle)) - point[1] * Math.sin(angle), (point[0] * Math.sin(angle)) + point[1] * Math.cos(angle)];
   }
 }
