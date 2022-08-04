abc1c597436bcbc82ffcc86b9617cb1fde74d2fc
HarryStevens <harryjosephstevens@gmail.com>
2022-08-04 14:24:29 -0400

    pointTranslate returns input point if only one argument is passed

--- src/points/pointTranslate.js
+++ src/points/pointTranslate.js
@@ -1,7 +1,7 @@
 import { angleToRadians } from "../angles/angleToRadians";
 
 // Translates a point by an angle in degrees and distance
-export function pointTranslate(point, angle, distance){
+export function pointTranslate(point, angle = 0, distance = 0){
   const r = angleToRadians(angle);
   return [point[0] + distance * Math.cos(r), point[1] + distance * Math.sin(r)];
 }
--- test/pointTranslate-test.js
+++ test/pointTranslate-test.js
@@ -1,7 +1,7 @@
 const tape = require("tape"),
       geometric = require("../");
 
-tape("pointTranslate(point, angle, distance) translates a point by an angle in degrees and distance", function(test) {
+tape("pointTranslate(point, angle, distance) translates a point by an angle in degrees and distance", test => {
   test.equal(geometric.pointTranslate([0, 0], 0, 1)[0], 1);
   test.equal(geometric.pointTranslate([0, 0], 0, 1)[1], 0);
   
@@ -17,5 +17,12 @@ tape("pointTranslate(point, angle, distance) translates a point by an angle in d
   test.equal(geometric.pointTranslate([0, 0], 360, 1).map(d => Math.round(d))[0], 1);
   test.equal(geometric.pointTranslate([0, 0], 360, 1).map(d => Math.round(d))[1], 0);
 
+  test.end();
+});
+
+tape("pointTranslate returns input point if only one argument is passed", test => {
+  const point = [Math.random(), Math.random()]
+  test.deepEqual(geometric.pointTranslate(point), point);
+  
   test.end();
 });
