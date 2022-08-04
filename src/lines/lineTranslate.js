631b600a99021dab99165dab9b901e75badc929e
HarryStevens <harryjosephstevens@gmail.com>
2022-08-04 14:40:42 -0400

    Add description of lineTranslate in comment

--- src/lines/lineTranslate.js
+++ src/lines/lineTranslate.js
@@ -1,5 +1,6 @@
 import { pointTranslate } from "../points/pointTranslate";
 
+// Returns the coordinates resulting from translating a line by an angle in degrees and a distance.
 export function lineTranslate(line, angle, distance){
   return line.map(point => pointTranslate(point, angle, distance));
 }
