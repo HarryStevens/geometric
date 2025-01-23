0b635d3ae28d1550ad49bcb9061e4fff2611172b
HarryStevens <harryjosephstevens@gmail.com>
2025-01-23 15:39:47 -0500

    Move index files to directories

---
+++ src/index.js
@@ -0,0 +1,41 @@
+export { lineAngle } from "./lines/lineAngle";
+export { lineInterpolate } from "./lines/lineInterpolate";
+export { lineLength } from "./lines/lineLength";
+export { lineMidpoint } from "./lines/lineMidpoint";
+export { lineRotate } from "./lines/lineRotate";
+export { lineTranslate } from "./lines/lineTranslate";
+
+export { pointRotate } from "./points/pointRotate";
+export { pointTranslate } from "./points/pointTranslate";
+
+export { polygonArea } from "./polygons/polygonArea";
+export { polygonBounds } from "./polygons/polygonBounds";
+export { polygonCentroid } from "./polygons/polygonCentroid";
+export { polygonClose, polygonClosed } from "./polygons/polygonClose";
+export { polygonHull } from "./polygons/polygonHull";
+export { polygonInterpolate } from "./polygons/polygonInterpolate";
+export { polygonLength } from "./polygons/polygonLength";
+export { polygonMean } from "./polygons/polygonMean";
+export { polygonRandom } from "./polygons/polygonRandom";
+export { polygonReflectX } from "./polygons/polygonReflectX";
+export { polygonReflectY } from "./polygons/polygonReflectY";
+export { polygonRegular } from "./polygons/polygonRegular";
+export { polygonRotate } from "./polygons/polygonRotate";
+export { polygonScale } from "./polygons/polygonScale";
+export { polygonScaleArea } from "./polygons/polygonScaleArea";
+export { polygonScaleX } from "./polygons/polygonScaleX";
+export { polygonScaleY } from "./polygons/polygonScaleY";
+export { polygonTranslate } from "./polygons/polygonTranslate";
+export { polygonWind } from "./polygons/polygonWind";
+
+export { lineIntersection } from "./relationships/lineIntersection";
+export { lineIntersectsPolygon } from "./relationships/lineIntersectsPolygon";
+export { pointInPolygon } from "./relationships/pointInPolygon";
+export { pointOnPolygon } from "./relationships/pointOnPolygon";
+export { pointLeftofLine, pointRightofLine, pointOnLine, pointWithLine } from "./relationships/pointOnLine";
+export { polygonInPolygon } from "./relationships/polygonInPolygon";
+export { polygonIntersectsPolygon } from "./relationships/polygonIntersectsPolygon";
+
+export { angleReflect } from "./angles/angleReflect";
+export { angleToDegrees } from "./angles/angleToDegrees";
+export { angleToRadians } from "./angles/angleToRadians";
